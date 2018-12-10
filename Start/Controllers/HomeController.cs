7289307bffb0d396023c79d7a4317135143a84e8
widlund_martin <widlund_martin@hotmail.com>
2018-12-10 14:06:36 +0100

    Start er færdig.

---
+++ Start/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Start.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Index2()
+        {
+
+            string navn = "kjkjk";
+
+            return Json(navn, JsonRequestBehavior.AllowGet);
+
+        }
+    }
+}
+
